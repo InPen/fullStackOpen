5dbc47713d213cfdddb38022566d2180ac864ac6
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-02 16:34:57 -0500

    Include new components

--- 1/unicafe/src/index.js
+++ 1/unicafe/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = () => {}
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.course}</h1>
+    </header>
+  )
+}
 
-const Content = () => {}
+const Content = (props) => {
+  console.log(props);
+  return (
+    <p>The topic of the course is {props.part} and there are {props.exercises} exercises</p>
+  )
+}
 
-const Total = () => {}
+const Total = (props) => {
+  return (
+    <p>In total this course has {props.total} exercises</p>
+  )
+}
 
 const App = () => {
   const course = 'Half stack application development'
@@ -18,17 +33,11 @@ const App = () => {
 
   return (
     <div>
-      <h1> {course} </h1>
-      <p>
-      {part1} {exercises1}
-      </p>
-      <p>
-      {part2} {exercises2}
-      </p>
-      <p>
-      {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1+exercises2+exercises3}</p>
+      <Header  course={course}/>
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total total={exercises1+exercises2+exercises3}/>
     </div>
   )
 }
