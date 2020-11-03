bc837f68acef3f6e130a5e1461426739ccddb6ad
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-02 20:40:56 -0500

    make all variables into a big object array thingy

--- 1/unicafe/src/index.js
+++ 1/unicafe/src/index.js
@@ -23,13 +23,14 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half stack application development'
-  const parts = [
+  const course = {
+    name:'Half stack application development',
+    parts : [
     {name:'Fundamentals of React', exercises: 10},
     {name:'Using props to pass data', exercises:7},
     {name:'State of a component', exercises:14}
   ]
-
+}
 
   return (
     <div>
