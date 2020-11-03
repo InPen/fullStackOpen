5d558633eb5b7c4a3f15b63ef5576ae0a5724143
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-03 12:54:27 -0500

    Simplify variables even more

--- 1/part1/src/index.js
+++ 1/part1/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Hello = (props) => {
-  const name = props.name
-  const age = props.age
+  const {name, age} = props
 
   const bornYear = () => {
     const yearNow = new Date().getFullYear()
