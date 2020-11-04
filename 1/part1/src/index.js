591fde93c17212ae0bd220153b4201ae79e6bfe3
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-04 12:54:51 -0500

    Save L && R clicks in 2 separate arrays to be later on concatinated into one Every click is stored into a separate piece of state called allClicks that is initialized as an empty array

--- 1/part1/src/index.js
+++ 1/part1/src/index.js
@@ -14,23 +14,26 @@ const Button = ({handleClick, text}) => {
 }
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left:0, right:0
-  })
-  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
-    // const newClicks = {
-    //   right: clicks.right +1,
-    //   left: clicks.left
-    // }
-    // setClicks(newClicks)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   return (
     <div>
-    {clicks.left}
+    {left}
       <button onClick={handleLeftClick}> Left </button>
       <button onClick={handleRightClick}> Right </button>
-    {clicks.right}
+    {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 
