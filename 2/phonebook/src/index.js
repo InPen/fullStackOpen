78ebfcabe34661dd62f536b89b8cc2704d9fbb5c
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-17 13:58:26 -0500

    Include add contact functionality

--- 2/phonebook/src/index.js
+++ 2/phonebook/src/index.js
@@ -1,21 +1,30 @@
 import React, { useState } from 'react'
 
-const App = () => {
+const App = (props) => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ])
   const [ newName, setNewName ] = useState('')
 
+  const addContact = (event) => {
+    event.preventDefault()
+    const personObject = {
+      content : newName,
+      date : new Date().toISOString(),
+      id : persons.length + 1,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">Add New Contact</button>
-        </div>
+      <form onSubmit={addContact}>
+        <input
+          value={newName}
+        />
+        <button type="submit">Add New Contact</button>
       </form>
       <h2>Numbers</h2>
       ...
