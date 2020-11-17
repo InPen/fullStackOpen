404a2d31389d742822956b3fa26a37d35bf707a4
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-16 20:09:12 -0500

    Put App in a separate file

--- 2/courseinfo/course/src/App.js
+++ 2/courseinfo/course/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import Note from './components/Note'
+import Course from './components/Course'
 
-const App = ({ notes }) => {
+
+const App = ({ course }) => {
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) =>
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-    </div>
+    <Course course={course} />
   )
 }
 
