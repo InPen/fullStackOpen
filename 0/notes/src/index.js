93a71f162ad6344166ba08a2f177532c7abb5834
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-17 19:54:03 -0500

    Refactor code

--- 0/notes/src/index.js
+++ 0/notes/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  console.log(notes)
-})
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+  })
 
 const notes = [
   {
