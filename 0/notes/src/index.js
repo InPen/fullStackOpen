6866b771d68999a880aa384fd1c4ac8426623987
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-17 19:52:11 -0500

    Create GET route

--- 0/notes/src/index.js
+++ 0/notes/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
+const promise = axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  console.log(notes)
+})
 
 const notes = [
   {
