b3807257b7a7c64292d25e6b3a0ce2ffb0ba1f14
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-19 22:00:08 -0500

    4. Import useState since our app will be dynamic and will show different things depending on user interaction

--- 2/countries/src/index.js
+++ 2/countries/src/index.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Countries = (props) => {
   return (
     <div>
-      <p>I don't have papers {props.name} </p>
+      <p>find countries {props.name} </p>
     </div>
   )
 }
 
-const App = () => (
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+  return (
   <div>
-    <Countries name='por pendeja' />
-    <Countries />
-    <Countries />
-    <Countries />
-    <Countries />
-    <Countries />
+    <Countries name='Switzerland' />
+    {counter}
   </div>
-)
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
