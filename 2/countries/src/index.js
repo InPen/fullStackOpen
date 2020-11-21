e7c5c7fa548a1498ce7916ecaf0995c3bc99091a
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-21 10:24:51 -0500

    7. Import App

--- 2/countries/src/index.js
+++ 2/countries/src/index.js
@@ -1,36 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
-const Country = (props) => {
-  return (
-    <div>
-      <p>find countries {props.name} </p>
-    </div>
-  )
-}
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  const setToZero = () => setCounter(0)
-
-  return (
-  <div>
-    <Country name='Switzerland' />
-    {counter}
-    <Button handleClick={setToZero} text='show' />
-  </div>
-  )
-}
 
 ReactDOM.render(<App />, document.getElementById('root'))
