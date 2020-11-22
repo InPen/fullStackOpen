fdec88dc9aad273f7145731e4261d314c6c9f2c2
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-21 22:58:14 -0500

    Include weather api GET

--- 2/countries/src/App.js
+++ 2/countries/src/App.js
@@ -1,14 +1,15 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-// import InputField from './components/InputField'
-// import Countries from './components/Countries'
+import "./App.css";
+import InputField from "./components/InputField";
+import Countries from "./components/Countries";
 
 const App = (props) => {
 //search is an empty string. setSearch is a function used to update search
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
-  // const [weatherData, setWeatherData] = useState(null)
+  const [weatherData, setWeatherData] = useState(null)
 
 //GET country data from api
   useEffect(() => {
@@ -23,27 +24,47 @@ const App = (props) => {
       })
   }, [search])
 
+// GET weather info from api
+  useEffect(() => {
+    const baseUrl = "http://api.weatherstack.com/current";
+    const ACCESS_KEY = process.env.REACT_APP_API_KEY;
+    if (countries.length === 1) {
+      const capital = countries.map(country => country.capital);
+      if (capital[0]) {
+        axios
+          .get(`${baseUrl}?access_key=${ACCESS_KEY}&query=${capital[0]}`)
+          .then(response => {
+            setWeatherData(response.data);
+          });
+      }
+    }
+  }, [countries]);
 
-  const findCountry = (event) => {
+  const handleSearchChange = (event) => {
     event.preventDefault()
     setSearch(event.target.value)
   }
+  const handleClick = (countryName) => {
+    setSearch(countryName)
+  }
 
   return (
-  <div>
-    <p>find countries</p>
-    <form onSubmit={showCountry}>
-      <input
+    <main className="App">
+      {/* Search for country data by name */}
+      <InputField
+        htmlFor="search"
+        type="text"
+        label="Find countries"
         value={search}
-        onChange={findCountry}
+        onChange={handleSearchChange}
       />
-      <button type="submit">search</button>
-    </form>
-    <ul>
-    {countries.map(country => <h2>{country.name}</h2>)}
-    </ul>
-  </div>
-  )
-}
+      <Countries
+        handleClick={handleClick}
+        countries={countries}
+        weatherData={weatherData}
+      />
+    </main>
+  );
+};
 
-export default App
+export default App;
