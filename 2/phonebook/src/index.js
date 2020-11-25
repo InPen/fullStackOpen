db9e081c99399ee5e8f65a1dd5187f7c06530529
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-11-25 16:40:13 -0500

    Create App component

--- 2/phonebook/src/index.js
+++ 2/phonebook/src/index.js
@@ -1,51 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-const App = (props) => {
-  const [ persons, setPersons ] = useState([])
-  const [ newName, setNewName ] = useState('')
-  const [showAll, setShowAll] = useState(true)
-
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        const persons = response.data
-        console.log(persons)
-      })
-    }, [])
-console.log('render', persons.length, 'persons')
-
-  const addContact = (event) => {
-    event.preventDefault()
-    const personObject = {
-      content : newName,
-      date : new Date().toISOString(),
-      id : persons.length + 1,
-    }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-  }
-
-  const handleContactChange = (event) =>{
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addContact}>
-        <input
-          value={newName}
-        />
-        <button type="submit">Add New Contact</button>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
-}
-
-export default App
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
