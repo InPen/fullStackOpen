eb1ffbc4589874dc363582f505961dbde43a15ea
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-12-06 07:47:47 -0500

    Make the app a web server

--- 3/phonebook/index.js
+++ 3/phonebook/index.js
@@ -1,1 +1,10 @@
-console.log('hewo :3')
+const http = require('http')
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('Hewo :3')
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
